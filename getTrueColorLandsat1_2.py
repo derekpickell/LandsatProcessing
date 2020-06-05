44de107078103bbb5c2628c6e98f72842d5df65f
derekpickell <37055625+derekpickell@users.noreply.github.com>
2020-06-04 19:51:27 -0700

    changes

---
+++ getTrueColorLandsat1_2.py
@@ -0,0 +1,150 @@
+import cv2 as cv
+import sys, os
+import tkinter as tk
+#from tkinter import filedialog
+from PyQt5.Qt import *
+from PyQt5.QtWidgets import *
+from landsatCore import getTrueColorLandsat, colorCorrect, convertToFinal
+
+# GUI: LANDSAT TRUE COLOR IMAGE CONVERTER
+# V1.0 June 1 2020
+# CREATED BY DEREK PICKELL
+
+def run(folder):
+    """
+    Functionality: executes main processing sequence using core landsat library
+    """
+    try:
+        button2.setText("processing...")
+        print("merging images...")
+        print(folder)
+        mergedImage = getTrueColorLandsat(folder)
+        print("color correcting...")
+        B, G, R = cv.split(mergedImage)
+        Bnew = colorCorrect(B)
+        Gnew = colorCorrect(G)
+        Rnew = colorCorrect(R)
+        colorCorrected = cv.merge((Bnew, Gnew, Rnew))
+        print("saving image...")
+        convertToFinal(colorCorrected, folder)
+        print("processing complete")
+        button2.setText("processing complete...")
+        
+    except:
+        print("failure to launch")
+        #text2.set("failure to launch...")
+
+def restart():
+    """
+    Functionality: Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+folder = [None]
+def fileDialog():
+    folderPath = QFileDialog.getExistingDirectory(window, "open dir")
+    if folderPath:
+        folder[0] = folderPath
+        button1.setText(folderPath)
+
+    # text2.set("Press to Start")
+    # if getattr(sys, 'frozen', False): # check if running bundle or normal python
+    #     application_path = os.path.dirname(sys.argv[0])
+    # else: # normal python
+    #     application_path = os.path.dirname(os.path.abspath(__file__))
+    # folderPath = filedialog.askdirectory(initialdir =  str(application_path), title = "Select Folder")
+    # if folderPath:
+    #     text.set(folderPath)
+    #     folder[0] = folderPath
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle("Landsat 8 Image Processing")
+
+label1 = QLabel()
+label1.setText("Created by Derek Pickell\nClick to search for folder that contains \
+proper images.\n Required: Landsat 8 Bands 4, 3, 2, 8 and metadata.txt. \nOutput is a pansharpened RGB \
+true color image \nstored in the original folder.")
+label1.setAlignment(Qt.AlignCenter)
+
+
+button1 = QPushButton("Search for Landsat image folder")
+button1.clicked.connect(lambda: fileDialog())
+
+button2 = QPushButton("Press to Start")
+button2.clicked.connect(lambda: run(folder))
+
+label2 = QLabel()
+label2.setText("Depending on datasize, processing can take several minutes \nwith the spinning wheel of death.")
+label2.setAlignment(Qt.AlignCenter)
+
+button3 = QPushButton("Reset")
+button3.clicked.connect(lambda: restart())
+
+label3 = QLabel()
+label3.setText("V.6.2020. Distribute with credit to www.derekpickell.com")
+label3.setAlignment(Qt.AlignCenter)
+
+vbox = QVBoxLayout()
+vbox.addWidget(label1)
+vbox.addStretch()
+vbox.addWidget(button1)
+vbox.addStretch()
+vbox.addWidget(button2)
+vbox.addStretch()
+vbox.addWidget(label2)
+vbox.addStretch()
+vbox.addWidget(button3)
+vbox.addStretch()
+vbox.addWidget(label3)
+
+# l1.setOpenExternalLinks(True)
+# l4.linkActivated.connect(clicked)
+# l2.linkHovered.connect(hovered)
+# l1.setTextInteractionFlags(Qt.TextSelectableByMouse)
+window.setLayout(vbox)
+   
+window.show()
+sys.exit(app.exec_())
+
+# window = tk.Tk()
+# window.title("Landsat Image Processing")
+# window.geometry("500x500")
+# #window.resizable(0, 0)
+# greeting = tk.Label(text="Created by Derek Pickell\nClick to search for folder that contains \
+# proper images.\n Required: Bands 4, 3, 2, 8 and metadata.txt. \nOutput is a pansharpened RGB \
+# true color image \nstored in the original folder.")
+# greeting.grid(column = 0, row = 1, padx = 5, pady = 5)
+
+# text = tk.StringVar()
+# button1 = tk.Button(textvar = text, command =  lambda: fileDialog(), bg = "blue")
+# text.set("Search for Landsat image folder")
+# button1.grid(column = 0, row = 2, padx = 20, pady = 20)
+
+# text2 = tk.StringVar()
+# button2 = tk.Button(textvar = text2, command = lambda: run(folder))
+# text2.set("Press to start")
+# button2.grid(column = 0, row = 3, padx = 20, pady = 20)
+
+# text3 = tk.StringVar()
+# label = tk.Label(textvar =text3)
+# text3.set("Depending on datasize, processing can take several minutes \nwith spinning wheel of death. \nGo do something else in meantime.")
+# label.grid(column = 0, row = 4, padx = 20, pady = 20)
+
+# button3 = tk.Button(text = "Restart", command = lambda: restart())
+# button3.grid(column = 0, row = 5, padx = 20, pady = 20)
+
+# label = tk.Label(text = "Created 2020. Distribute as you please, with credit to www.derekpickell.com")
+# label.grid(column = 0, row = 6, pady = 20)
+
+# window.grid_columnconfigure(0,weight=1)
+# window.grid_rowconfigure(1,weight=1)
+# window.grid_rowconfigure(2,weight=1)
+# window.grid_rowconfigure(3,weight=1)
+# window.grid_rowconfigure(4,weight=1)
+# window.grid_rowconfigure(5,weight=1)
+# window.grid_rowconfigure(6,weight=1)
+
+# window.mainloop() 
---
+++ landsatCore.py
@@ -0,0 +1,127 @@
+import cv2 as cv
+import glob
+import sys, os
+import numpy as np
+
+# CORE LIBRARY: LANDSAT TRUE COLOR IMAGE CONVERTER 
+# V1.0 June 1 2020
+# CREATED BY DEREK PICKELL
+
+def getTrueColorLandsat(folder):
+    """
+    Functionality: Converts B&W satellite data into true color image
+    Inputs: LANDSAT bands 2-4 and panchromatic band (band 8)
+    Outputs: 'HSV sharpened' true color image
+    """
+    folder = str(folder[0])
+    print("folder location:", folder)
+
+    path4 = os.path.join(folder, "*4.TIF") 
+    band4 = glob.glob(path4)
+    path3 = os.path.join(folder, "*3.TIF")
+    band3 = glob.glob(path3)
+    path2 = os.path.join(folder, "*2.TIF")
+    band2 = glob.glob(path2)
+    pathP = os.path.join(folder, "*8.TIF")
+    panchromatic = glob.glob(pathP)
+    lists = [str(band4[0]), str(band3[0]), str(band2[0]), str(panchromatic[0])]
+
+    # READ IMAGES
+    blueImage = cv.imread(lists[0], -1) #-1 flag = "image as is"
+    greenImage = cv.imread(lists[1], -1)
+    redImage = cv.imread(lists[2], -1)
+    panImage = np.array(cv.imread(lists[3],-1), dtype=np.float32)
+    panImage /=655.35
+    x = panImage.shape[0]
+    y = panImage.shape[1]
+    
+    # IMAGE PROCESSING
+    BGR = cv.merge((redImage, greenImage, blueImage)) # merge 3 channels, reads RGB but OpenCV operates in BGR
+    BGRresized = cv.resize(BGR, (y, x), interpolation = cv.INTER_LINEAR) # interpolate image size bilinearly for 15m resolution
+    img = np.array(BGRresized, dtype=np.float32)
+    img /= 65535.0 # scale color image to 32 bit float
+
+    labColor = cv.cvtColor(img, cv.COLOR_BGR2Lab) # convert to LAB color
+    L,A,B=cv.split(labColor) # split into channels 
+    labColorMerge = cv.merge((panImage, A, B)) # replace intensity channel with 15m band  
+    mergedImage = cv.cvtColor(labColorMerge, cv.COLOR_Lab2BGR) # convert back to RGB  
+    print("LAB max pixel intensity: ",np.amax(labColor)) # max pixel intensities should be about equal
+    print("Sharpened LAB max pixel intensity:", np.amax(labColorMerge))
+    cv.imshow("merged image 1", mergedImage)
+    return mergedImage
+
+
+def colorCorrect(colorChannel):
+    """
+    Functionality: Adjusts Black point for RGB channels to better mirror "true color" 
+        and make post-processing easier 
+    Input: the merged RGB or LAB Image
+    Output: White balanced and lightened image for easier final processing
+    """
+    histogram, bins = np.histogram(colorChannel.ravel(), 256,[0,1]) # create histogram scaled 0-255
+    histogram = histogram[1:] # crop out pure black values
+    maxVal = histogram.max() 
+    threshold = 0.3*maxVal # threshold 1/3 of max value: all lower bins are cropped
+
+    for i in range(0, len(histogram)):
+        if histogram[i] > threshold:
+            index = i 
+            break
+
+    minVal = index/256 # divide by 256 since float32 type; gives min pix val that isn't cropped
+    print("crop index:", index)
+    scaledMatrix = np.ravel(colorChannel) - minVal # shift all pixel values left
+    croppedMatrix = np.clip(scaledMatrix, 0, None) # negative values floored to 0
+    shape = colorChannel.shape
+    shapedChannel = np.reshape(croppedMatrix, shape) # reshape to original form
+    #plt.hist(newB.ravel(),256,[0,1]); plt.show()
+    
+    return shapedChannel
+
+
+def convertToFinal(outputImage, folder):
+    """
+    Functionality: Gets name from metadata and outputs final image product
+    Input: color corrected LAB image
+    Output: RBG image
+    """
+    #if getattr(sys, 'frozen', False): # check if running bundle or normal python
+    #    application_path = os.path.dirname(sys.argv[0])
+    #else: # normal python
+    #application_path = os.path.dirname(os.path.abspath(__file__))
+
+    folder = str(folder[0])
+    pathDate = os.path.join(folder, "*.TIF") 
+    #pathDate = os.path.join(application_path, "*.txt")
+    dateTitle = glob.glob(pathDate)
+    date = str(dateTitle[0]).split("_")
+    #dirname = os.path.dirname(os.path.abspath(__file__))
+    title = date[0] + "_" + date[1] + "_" + date[2] + "_" + date[3] + "_" + 'OUTPUT.TIF' 
+    path = os.path.join(folder, title) 
+    print(path)
+    cv.imshow("mergedImage", margedImage)   
+    cv.imwrite(path, outputImage) 
+    cv.waitKey(0)
+
+if __name__=="__main__":
+    application_path = os.path.dirname(os.path.abspath(__file__))
+    folder = [application_path]
+    try:
+        print("merging images...")
+        print(application_path)
+        mergedImage = getTrueColorLandsat(folder)
+        cv.imshow("mergedImage", margedImage)
+        print("color correcting...")
+        B, G, R = cv.split(mergedImage)
+        Bnew = colorCorrect(B)
+        Gnew = colorCorrect(G)
+        Rnew = colorCorrect(R)
+        colorCorrected = cv.merge((Bnew, Gnew, Rnew))
+        cv.imshow("mergedImage", colorCorrected)
+        print("saving image...")
+        convertToFinal(colorCorrected, folder)
+        print("processing complete")
+        cv.waitKey(0)
+        
+    except:
+        print("failure to launch")
